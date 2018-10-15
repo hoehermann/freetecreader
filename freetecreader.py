cb4c208c95c357e6d1021fa1bea6f5594d6cdeaa
Hermann Höhne <hoehermann@gmx.de>
2018-10-15 22:56:40 +0200

    Data is now actually read from the device by default.

--- freetecreader.py
+++ freetecreader.py
@@ -24,17 +24,17 @@ def request(hd, addr = None, length = None, msg_bytes = None):
 def create_dump(hd):
     length = 32
     first_addr = 0x0000
-    last_addr = 0x2000 # actually 0xFFFF
+    last_addr = 0xFFFF
     answer = b''
     for addr in range(first_addr, last_addr, length):
         answer += request(hd, addr, length)
-        sys.stdout.write("\rReading from device… %d%%"%(int(addr/last_addr*100)))
-    sys.stdout.write("\nDone.\n")
+        sys.stderr.write("\rReading from device… %d%%"%(int(addr/last_addr*100)))
+    sys.stderr.write("\nDone.\n")
     return answer
 
 
 mmap = {
-    "init ok"       : (0x00, 2),
+    "init_ok"       : (0x00, 2),
     "model"         : (0x02, 2),
     "ID"            : (0x05, 4),
     "settings"      : (0x09, 6),
@@ -57,8 +57,12 @@ def get_series_dates(data):
             date = [binascii.b2a_hex(bytes([b])).decode("ascii") for b in date]
             year, month, day, hour, minute, second = [int(d) for d in date]
             yield(datetime.datetime(2000+year, month, day, hour, minute, second))
-            #yield("20%s-%s-%s %s:%s:%s"%tuple(date))
     return convert_dates(get_field(data, "series_dates"))
+    
+def convert_measurement(measurement):
+    humidity = int(measurement[0])-20
+    temperature = int.from_bytes(measurement[1:3], byteorder='big')*0.1-50
+    return (humidity, temperature)
 
 if __name__ == "__main__":
     vendor_id = 0x10c4
@@ -66,41 +70,36 @@ if __name__ == "__main__":
     timeout_ms = 1000
     hd = hidapi.Device(vendor_id=vendor_id, product_id=product_id)
 
-    #data = create_dump(hd)
+    data = create_dump(hd)
     #with open("dump.bin",'wb') as f:
     #    f.write(data)
-    data = open("dump.bin",'rb').read()
+    #data = open("dump.bin",'rb').read()
 
-    #for field in mmap.keys():
-    #    if "series" not in field:
-    #        print("%s: %s"%(field, binascii.b2a_hex(get_field(data,field)).decode("ascii")))
+    if (int.from_bytes(get_field(data, "init_ok"), byteorder='big') != 0x55aa):
+        raise RuntimeError("Incorrect device ROM magic number. This software is not meant to be used with your device.")
+    if (int.from_bytes(get_field(data, "model"), byteorder='big') != 0x0201):
+         raise RuntimeError("Unknown model number.")
+    
+    sys.stderr.write("Device ID: %s\n"%(binascii.b2a_hex(get_field(data, "ID")).decode("ascii")))
+
+    if False:
+        for field in mmap.keys():
+            if "series" not in field:
+                print("%s: %s"%(field, binascii.b2a_hex(get_field(data, field)).decode("ascii")))
 
     series_counts = get_field(data, "series_counts")
     series_counts = [sc for sc in series_counts if sc != 0xFF]
     series_dates = get_series_dates(data)
-    #for byte in series_counts:
-    #    print(byte)
-    #for date in series_dates:
-    #    print(date)
-    #print(list(zip(series_dates, series_counts)))
-    def convert_measurement(measurement):
-        humidity = int(measurement[0])-20
-        temperature = int.from_bytes(measurement[1:3], byteorder='big')*0.1-50
-        return (humidity, temperature)
     measurements = get_chunks(data[mmap["series"][0]:], 3)
     number = 0
     print("Nummer	Aufzeichnungszeit	Temperatur(°C)	Luftfeuchtigkeit(%)\r")
     for series_count, series_start_date in zip(series_counts, series_dates):
         for i in range(64):
             humidity, temperature = convert_measurement(next(measurements))
-            #print(number, series_start_date, i, binascii.b2a_hex(next(measurements)))
             if (i > series_count):
                 pass
-                #print("^^ ignored")
             else:
                 pass
                 number += 1
                 measurement_date = series_start_date+datetime.timedelta(minutes=5*i)
                 print(" %d\t %s\t %.1f\t %d\r"%(number, measurement_date, temperature, humidity))
-                
-                
