34f96b5049c3e25000ca0e045a9c1f5962a70a4c
Hermann Höhne <hoehermann@gmx.de>
2018-10-15 21:55:26 +0200

    Works up to gap in measurements.

--- freetecreader.py
+++ freetecreader.py
@@ -3,6 +3,7 @@
 import hidapi
 import binascii
 import sys
+import datetime
 
 def request(hd, addr = None, length = None, msg_bytes = None):
     if (not msg_bytes):
@@ -31,22 +32,72 @@ def create_dump(hd):
     sys.stdout.write("\nDone.\n")
     return answer
 
+
+mmap = {
+    "init ok"       : (0x00, 2),
+    "model"         : (0x02, 2),
+    "ID"            : (0x05, 4),
+    "settings"      : (0x09, 6),
+    "unknown"       : (0x50, 6),
+    "series_counts" : (0x58, 323), # TODO: find out actual length (this is a guess)
+    "series_dates"  : (0x19C, 2915), # TODO: find out actual length (this is a guess)
+    "series"        : (0xD00, 0xFFFF-0xD00) # TODO: find out actual length (this is a guess)
+}
+def get_field(data, field):
+    addr, length = mmap[field]
+    return data[addr:addr+length]
+
+def get_chunks(data, size):
+    return (data[i*size:(i+1)*size] for i in range(len(data)//size))
+    
+def get_series_dates(data):
+    def convert_dates(series_dates):
+        for date in get_chunks(series_dates, 8):
+            date = date[:-2]
+            date = [binascii.b2a_hex(bytes([b])).decode("ascii") for b in date]
+            year, month, day, hour, minute, second = [int(d) for d in date]
+            yield(datetime.datetime(2000+year, month, day, hour, minute, second))
+            #yield("20%s-%s-%s %s:%s:%s"%tuple(date))
+    return convert_dates(get_field(data, "series_dates"))
+
 if __name__ == "__main__":
     vendor_id = 0x10c4
     product_id = 0x8468
     timeout_ms = 1000
     hd = hidapi.Device(vendor_id=vendor_id, product_id=product_id)
 
-    data = create_dump(hd)
-    with open("dump.bin",'wb') as f:
-        f.write(data)
+    #data = create_dump(hd)
+    #with open("dump.bin",'wb') as f:
+    #    f.write(data)
+    data = open("dump.bin",'rb').read()
 
-    mmap = {
-        "init ok"  : (0x00, 2),
-        "model"    : (0x02, 2),
-        "ID"       : (0x05, 4),
-        "settings" : (0x09, 6)
-    }
-    #for field, (addr, length) in mmap.items():
-    #    answer = request(hd, addr, length)
-    #    print("%s: %s"%(field, binascii.b2a_hex(answer)))
+    #for field in mmap.keys():
+    #    if "series" not in field:
+    #        print("%s: %s"%(field, binascii.b2a_hex(get_field(data,field)).decode("ascii")))
+
+    series_counts = get_field(data, "series_counts")
+    series_counts = [sc for sc in series_counts if sc != 0xFF]
+    series_dates = get_series_dates(data)
+    #for byte in series_counts:
+    #    print(byte)
+    #for date in series_dates:
+    #    print(date)
+    #print(list(zip(series_dates, series_counts)))
+    def convert_measurement(measurement):
+        humidity = int(measurement[0])-20
+        temperature = int.from_bytes(measurement[1:3], byteorder='big')*0.1-50
+        return (humidity, temperature)
+    measurements = get_chunks(data[mmap["series"][0]:], 3)
+    number = 0
+    print("Nummer	Aufzeichnungszeit	Temperatur(°C)	Luftfeuchtigkeit(%)\r")
+    for series_start_date, series_count in zip(series_dates, series_counts):
+        for i in range(64):
+            if (i > series_count):
+                print("ignored")
+            else:
+                number += 1
+                measurement_date = series_start_date+datetime.timedelta(minutes=5*i)
+                humidity, temperature = convert_measurement(next(measurements))
+                print(" %d\t %s\t %.1f\t %d\r"%(number, measurement_date, temperature, humidity))
+                #print(series_start_date, i, binascii.b2a_hex(next(measurements)))
+                
